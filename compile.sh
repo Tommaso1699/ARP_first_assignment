fcdad5b3c3351156ed0c047c012fabdf26a45434
drozdja <drozdja@gmail.com>
2022-12-26 11:28:24 +0100

    deleted unused files

--- compile.sh
+++ compile.sh
@@ -4,5 +4,3 @@ gcc -o bin/inspection src/inspection_console.c -Iinclude -lncurses -lm
 gcc -o bin/command src/command_console.c -Iinclude -lncurses
 gcc -o bin/master src/master.c -Iinclude
 gcc -o bin/select_motor_signals src/Select_motor_signals.c -Iinclude
-# gcc -o bin/real_world src/Real_World.c -Iinclude
-# gcc -o bin/watchdog src/watchdog.c -Iinclude
